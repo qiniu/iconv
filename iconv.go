c5526698d0fe56515b1b0604431abf7f3f8ac80a
xushiwei <xushiweizh@gmail.com>
2020-06-20 04:22:46 +0800

    Update iconv.go

--- iconv.go
+++ iconv.go
@@ -6,7 +6,6 @@ package iconv
 // #cgo darwin  LDFLAGS: -liconv
 // #cgo freebsd LDFLAGS: -liconv
 // #cgo windows LDFLAGS: -liconv
-// #cgo linux LDFLAGS: -liconv
 // #include <iconv.h>
 // #include <stdlib.h>
 // #include <errno.h>
